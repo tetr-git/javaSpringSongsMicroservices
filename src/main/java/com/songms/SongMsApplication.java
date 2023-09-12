7d7faab9d1bcff2f75728b4d94b7605f57d96743
sklose@posteo.net <sklose@posteo.net>
2023-09-12 13:39:07 +0200

    first commit

---
+++ src/main/java/com/songms/SongMsApplication.java
@@ -0,0 +1,13 @@
+package com.songms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongMsApplication.class, args);
+	}
+
+}
