346643e88bacf36d4bb98843614140aa1574fbde
sklose@posteo.net <sklose@posteo.net>
2023-09-13 16:28:55 +0200

    redo modules

--- api-gateway/src/test/java/com/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package com.user;
+package com.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserApplicationTests {
+class ApiGatewayApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
--- auth/src/main/java/com/auth/AuthApplication.java
+++ auth/src/main/java/com/auth/AuthApplication.java
@@ -1,13 +1,13 @@
-package com.user;
+package com.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 
 }
