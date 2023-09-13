872dd41d3a7d5aff727b6742375446272fe612e1
sklose@posteo.net <sklose@posteo.net>
2023-09-13 16:31:19 +0200

    added songs/songLists modules

---
+++ songLists/src/main/java/com/songlists/SongListsApplication.java
@@ -0,0 +1,13 @@
+package com.songlists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongListsApplication.class, args);
+	}
+
+}
---
+++ songLists/src/test/java/com/songlists/SongListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songlists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
