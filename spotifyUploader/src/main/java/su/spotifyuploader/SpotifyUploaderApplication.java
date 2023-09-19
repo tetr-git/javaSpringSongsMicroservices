574c29f723e02301c84da346d49834be59f9e668
sklose@posteo.net <sklose@posteo.net>
2023-09-19 18:41:02 +0200

    added spotify ms

---
+++ spotifyUploader/src/main/java/su/spotifyuploader/SpotifyUploaderApplication.java
@@ -0,0 +1,13 @@
+package su.spotifyuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyUploaderApplication.class, args);
+	}
+
+}
---
+++ spotifyUploader/src/test/java/su/spotifyuploader/SpotifyUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package su.spotifyuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
